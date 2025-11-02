8e4210e97ffcfc178ce954bb8796ea21642e3024
asrofilnadibs <asrofilnadibs28@gmail.com>
2025-11-02 10:59:51 +0700

    chore: last commit

--- public/js/app.js
+++ public/js/app.js
@@ -19437,8 +19437,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /mnt/windows-d/Proyek/Laravel/warehouse_pas/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /mnt/windows-d/Proyek/Laravel/warehouse_pas/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /mnt/windows-d/Proyek/Laravel/warehouse-pas/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /mnt/windows-d/Proyek/Laravel/warehouse-pas/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
